e7a2b1d955c824d9d12521cfdd6699ea21a7e677
larrythecow <sid@projekt-turm.de>
2012-09-26 08:23:46 +0200

    kernel setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
 PWD=`pwd`
+CACHE_PWD=$PWD/../var/cache
+
+echo -e "pwd: $PWD"
 
 # 1. do we need this script or just a simple documentation?
 # should have options like
 # --board panda --kernel stable --get/build/all
 
-function panda(){
+function panda-get(){
         echo -e "get pandaboard 3.2.xxx.xxx kernel with sample config\n"
-	wget -c 'https://launchpad.net/ubuntu/+archive/primary/+files/linux-ti-omap4_3.2.0-1412.16.tar.gz'
-	tar xfz linux-ti-omap4_3.2.0-1412.16.tar.gz
-	wget -c http://dev.gentoo.org/~armin76/arm/pandaboard/kconfig -O ubuntu-precise/.config
-	echo -e "you can build with e.g. whith\n"
-	echo -e "make ARCH=arm CROSS_COMPILE=armv7a-hardfloat-linux-gnueabi- uImage"
-	echo -e "make ARCH=arm CROSS_COMPILE=arm-none-eabi- uImage"
+	wget -c 'https://launchpad.net/ubuntu/+archive/primary/+files/linux-ti-omap4_3.2.0-1412.16.tar.gz' -P $CACHE_PWD
+	wget -c http://dev.gentoo.org/~armin76/arm/pandaboard/kconfig -O $CACHE_PWD/panda-config
+
+        echo -e "decompressing kernel"
+        tar xfz $CACHE_PWD/linux-ti-omap4_3.2.0-1412.16.tar.gz -C $PWD/panda
+        echo -e "copy config from var"
+        cp $CACHE_PWD/panda-config $PWD/panda/ubuntu-precise/.config	
+}
+
+function panda-make(){
+	cd $PWD/panda/ubuntu-precise/
+	make ARCH=arm CROSS_COMPILE=arm-none-eabi- uImage	
+        echo -e "you can build with e.g. whith\n"
+        echo -e "make ARCH=arm CROSS_COMPILE=armv7a-hardfloat-linux-gnueabi- uImage"
+        echo -e "make ARCH=arm CROSS_COMPILE=arm-none-eabi- uImage"
 }
 
 function toradex(){
@@ -30,10 +42,14 @@ case "$1" in
 	multistrap
 	exit 0 
 	;;
-"panda")
-	panda
+"panda-get")
+	panda-get
 	exit 0
 	;;
+"panda-make")
+        panda-make
+        exit 0
+        ;;
 "toradex")
 	toradex
 	exit 0
