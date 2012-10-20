3b69171d716a588de9b7921225a592306ff219ad
larrythecow <sid@projekt-turm.de>
2012-10-20 15:00:38 +0000

    blub

--- src/gpio-io/gpio_test.c
+++ src/gpio-io/gpio_test.c
@@ -9,13 +9,20 @@
 #include <asm/uaccess.h>
 #include "panda.h"
 
+#define DIV_HZ 1
+
 static void timer_oscillateur(unsigned long);
 static struct timer_list timer;
+
+unsigned long int i=0;
+
 
 static int __init init_gpio_test (void)
 {
 	int err;
 	
+	printk("mod-loaded\n");
+	printk("expires:%lu jiffies:%lu HZ:%d DIV_HZ:%d\n", jiffies + HZ/DIV_HZ, jiffies, HZ, DIV_HZ);	
 	if ((err = gpio_request(LED1, THIS_MODULE->name)) != 0) {
 		return err;
 	}
@@ -26,7 +33,7 @@ static int __init init_gpio_test (void)
 	init_timer (& timer);
 	timer.function = timer_oscillateur;
 	timer.data = 0;
-	timer.expires = jiffies + HZ/10;
+	timer.expires = jiffies + HZ/DIV_HZ;
 	add_timer(& timer);
 	
 	return 0; 
@@ -37,6 +44,7 @@ static void __exit exit_gpio_test (void)
 {
 	del_timer(& timer);
 	gpio_free(LED1);
+	printk("exit i=%lu\n", i);
 }
 
 
@@ -45,7 +53,8 @@ static void timer_oscillateur(unsigned long unused)
 	static int value = 0;
 	gpio_set_value(LED1, value);
 	value = 1 - value;
-	mod_timer(& timer, jiffies + HZ/00);
+	mod_timer(& timer, jiffies + HZ/DIV_HZ);
+	i++;
 }
 
 
