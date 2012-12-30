ca69f8e8527abbcf20884e1853a17000e70a5df3
larrythecow <sid@projekt-turm.de>
2012-12-30 05:29:29 +0000

    removed: sensor_c.c; added gplv2 to own source

--- gpio-freq/gpio_fcntl.h
+++ gpio-freq/gpio_fcntl.h
@@ -1,9 +1,21 @@
-/* 
- * File:   gpio_fcntl.h
- * Author: sid
- *
- * Created on November 9, 2012, 6:02 PM
- */
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+Author: imran shamshad
+Email: sid@projekt-turm.de
+*/
+
 
 #ifndef GPIO_FCNTL_H
 #define	GPIO_FCNTL_H
